c94f6da346d041a2bdfc606e24c338bd8899b89e
Muhidin <muhidinhukic1@gmail.com>
2020-11-14 17:42:16 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do "Hello, World!"
+
+    end
+
+    get '/name' do  "My name is Muhidin"
+    end
+
+    get '/hometown' do "My hometown is Gorazde"
+    end
+
+    get '/favorite-song' do  "My favorite song is ...."
+    end
+    
 end
